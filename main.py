c93c69326180b06d616c096b8620f5f977e9d18e
Dubvas <vasyadbnn@yandex.ru>
2021-12-22 12:46:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import datetime
+import uuid
+import scipy
+
+from flask import Flask, send_from_directory, jsonify, make_response, request
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+app = Flask(__name__, static_url_path="")
+
+total_memes = 7573
+memes_pack_size = 5
+total_clusters = 100
+
+mongodb_client = MongoClient('localhost', 27017)
+users: Collection = mongodb_client.application.numbers
+likes: Collection = mongodb_client.application.likes
+memes: Collection = mongodb_client.application.memes
+
+def new_user(user_id):
+    result = {"_id": str(user_id), "rec": {}, "vis": {}, "text": {}}
+    for i in range(total_clusters):
+        result["rec"][str(i)] = {"wins": 0, "loses": 0}
+        result["vis"][str(i)] = {"wins": 0, "loses": 0}
+        result["text"][str(i)] = {"wins": 0, "loses": 0}
+    return result
+
+@app.route("/static/<path:filename>")
+def file(filename):
+    return send_from_directory("static", filename)
+
+@app.route("/like_meme", methods=["POST"])
+def like_meme():
+    r_data = request.get_json(force=True)
+    user_id = request.cookies.get("user_id")
+    meme_id = r_data["meme_id"]
+    like = r_data["value"]
+    likes.insert_one({"_id": str(uuid.uuid4()), "user_id": user_id, "meme_id": meme_id, "like": like})
+    meme = memes.find_one({"_id": meme_id})
+    user = users.find_one({"_id": str(user_id)})
+    type_ = "loses"
+    if like == 1:
+        type_ = "wins"
+    user["rec"][str(meme["rec"])][type_] += 1
+    user["vis"][str(meme["vis"])][type_] += 1
+    user["text"][str(meme["text"])][type_] += 1
+    users.update_one({"_id": str(user_id)}, {"$set": user})
+    return ""
+
+def roll_cluster(probs):
+    max_prob_cluster = -1
+    max_prob = -1
+    for i in range(total_clusters):
+        a = probs[str(i)]["wins"]
+        b = probs[str(i)]["loses"]
+        distr = scipy.stats.beta(a + 1, b + 1)
+        curr_prob = distr()
+        if(curr_prob > max_prob):
+            max_prob = curr_prob
+            max_prob_cluster = i
+    return max_prob_cluster
+
+def get_meme_recs(user):
+    pick_vis = roll_cluster(user["vis"])
+    pick_rec = roll_cluster(user["rec"])
+    pick_text = roll_cluster(user["text"])
+    ret = list(memes.find({"$or": [{"rec": pick_rec}, {"vis": pick_vis}, {"text": pick_text}]}))
+    meme_ids = [x["_id"] for x in ret]
+    rem = likes.find({"meme_id": {"$in": meme_ids}, "user_id": str(user["_id"])})
+    rem_ids = {x["meme_id"] for x in rem}
+    ret = [x for x in ret if not (x["_id"] in rem_ids)]
+    return ret
+
+@app.route("/meme", methods=["POST"])
+def meme_handler():
+    user_id = request.cookies.get("user_id")
+    if not user_id:
+        return ""
+    element = users.find_one({"_id": str(user_id)})
+    if not element:
+        users.insert_one(new_user(user_id))
+        element = users.find_one({"_id": str(user_id)})
+    ret = get_meme_recs(element)
+    for i in range(4):
+        if len(ret) >= memes_pack_size:
+            break
+        ret.extend(get_meme_recs(element))
+    ret = ret[:5]
+    result = [{"meme_id": x["_id"], "liked": 0} for x in ret]
+
+    return jsonify(result)
+
+
+@app.route("/")
+def index():
+    result = make_response((send_from_directory("static", "index.html"), 200))
+    user_id = request.cookies.get("user_id")
+    if not user_id:
+        user_id = str(uuid.uuid4())
+        result.set_cookie("user_id", user_id, expires=datetime.datetime.now() + datetime.timedelta(days=365))
+
+    return result
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
---
+++ static/main.js
@@ -0,0 +1,158 @@
+var delt = 50
+var maxSteps = 5000 / delt
+var currSteps = 0
+var spacePressed = 0
+var exps = []
+var tr = 0.85
+var queueSize = 50
+var queueTrunc = 10
+var queuePos = 0
+var queue = []
+var memesPackSize = 5
+
+async function connectWebcam(){
+	let stream = await navigator.mediaDevices.getUserMedia({ video: {}});
+	let videoElement = $("#videoElement").get(0);
+	videoElement.srcObject = stream;
+	videoElement.play();
+}
+
+async function doPrediction(){
+	let isLoaded = !!faceapi.nets.tinyFaceDetector.params;
+	let videoElement = $("#videoElement").get(0);
+	if(!isLoaded || videoElement.paused || videoElement.ended){
+		console.log("Not Loaded!");
+		return setTimeout(doPrediction);
+	}
+
+	let inputSize = 512;
+	let threshold = 0.5;
+
+	let netOptions = new faceapi.TinyFaceDetectorOptions({inputSize, threshold});
+    console.log("before result");
+	let result = await faceapi.detectSingleFace(videoElement, netOptions).withFaceExpressions();
+
+	console.log("try predict");
+	if(result)
+		exps.push(result.expressions["happy"] >= tr);
+	else
+	    exps.push(0);
+	setTimeout(doPrediction);
+}
+
+async function loadModels(){
+	await faceapi.nets.tinyFaceDetector.load("/static/faceapi/model/tiny_face_detector_model-weights_manifest.json");
+	await faceapi.nets.faceExpressionNet.load("/static/faceapi/model/face_expression_model-weights_manifest.json");
+}
+
+async function showMeme(){
+    $("#meme").get(0).src = "/static/memes/" + queue[queuePos]["meme_id"] + ".jpg";
+    if(queue[queuePos]["liked"] == 1)
+        $("#like-button").attr("class", "red");
+    else
+        $("#like-button").attr("class", "gray");
+    console.log("current meme is " + queue[queuePos]["meme_id"]);
+}
+
+async function setLikeMeme(){
+    $.ajax({
+            type: "POST",
+            url: "/like_meme",
+            dataType: "json",
+            data: JSON.stringify({
+                "meme_id": queue[queuePos]["meme_id"],
+                "value": queue[queuePos]["liked"]
+            })
+    });
+}
+
+async function nextMeme(){
+    setLikeMeme();
+    currSteps = 0;
+    exps = [];
+    queuePos++;
+    if(queue.length - queuePos <= memesPackSize){
+        $.ajax({
+            type: "POST",
+            url: "/meme"
+        }).done((msg) => {
+            queue = queue.concat(msg);
+            if(queue.length > queueSize){
+                queuePos -= queueTrunc;
+                if(queuePos < 0)
+                    queuePos = 0;
+                queue.splice(0, queueTrunc);
+            }
+        })
+    }
+    if(queuePos >= queue.length)
+        queuePos = Math.max(0, queue.length - 1);
+    showMeme();
+}
+
+async function likeMeme(setMeme){
+    if(setMeme == 1)
+        queue[queuePos]["liked"] = 1;
+    else
+        queue[queuePos]["liked"] = 1 - queue[queuePos]["liked"];
+    showMeme();
+}
+
+async function prevMeme(){
+    setLikeMeme();
+    currSteps = 0;
+    exps = [];
+    queuePos--;
+    if(queuePos < 0) queuePos = 0;
+    showMeme();
+}
+
+document.addEventListener('keydown', (event) =>{
+    if(event.code == "Space") spacePressed = 1;
+});
+
+document.addEventListener('keyup', (event) =>{
+    if(event.code == "Space") spacePressed = 0;
+});
+
+async function memeScroller(){
+    if(!spacePressed){
+        currSteps++;
+        if(exps.includes(true)){
+            likeMeme(1);
+            exps = [];
+        }
+        if(currSteps > maxSteps){
+            currSteps = 0;
+            exps = [];
+            nextMeme();
+        }
+        $("#meme-line-progress").get(0).style.width = (currSteps * 100.0 / maxSteps).toString() + "%";
+    }
+    setTimeout(memeScroller, delt);
+}
+
+async function main(){
+	let cam = connectWebcam();
+	let models = loadModels();
+	console.log("loading ...");
+	await cam;
+	await models;
+	console.log("Loaded weights!");
+	await $.ajax({
+        type: "POST",
+        url: "/meme"
+    }).done((msg) => {
+        queue = queue.concat(msg);
+    })
+	setTimeout(doPrediction, 1000);
+	showMeme();
+    memeScroller();
+}
+
+$(document).ready(function(){
+    main();
+    $("#left-button").click(prevMeme);
+    $("#like-button").click(likeMeme);
+    $("#right-button").click(nextMeme);
+});
